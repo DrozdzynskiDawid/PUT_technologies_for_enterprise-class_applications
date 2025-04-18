c9a8430f53876cb1d11e4d4e4f1b000fd81df56b
Dawid <dawid.drozdzynski.g4@gmail.com>
2025-04-18 11:58:31 +0200

    Initial commit

---
+++ movies-frontend/src/components/MoviesList.jsx
@@ -0,0 +1,13 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+
+function MoviesList() {
+    return (
+        <ListGroup>
+            <ListGroup.Item>Inception</ListGroup.Item>
+            <ListGroup.Item>Interstellar</ListGroup.Item>
+            <ListGroup.Item>Man in Black</ListGroup.Item>
+        </ListGroup>
+    );
+}
+
+export default MoviesList;
